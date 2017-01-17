a3029913ea7e9b36f2056dc657a2a50c8210ed9a
David Trask <vischa1647@gmail.com>
2017-01-17 02:07:50 -0700

    Main program updated

--- core/src/main.ino
+++ core/src/main.ino
@@ -1,10 +1,23 @@
 /*
 * Written by: David Trask
-* 
-*
-*
-*
+* Created on: 2017-01-17
+* Last Updated: 2017-01-17
+* Version: 0.00.01
+* Last Update Notes:
+*   - Created initial file
+*   - Added base libraries
+*   - Created Main and Setup functions
 */
+
+//The various needed libraries.
+#include <SPI.h> // SPI?
+#include <Ethernet.h> //Ethernet Protocols Lib
+#include <PubSubClient.h> //MQTT Lib
+#include <Arduino.h> //General Arduino Commands for debugging serial.
+#include <Wire.h> //I2C Lib
+#include "Adafruit_MCP9808.h" //Adafruit Breakout board Lib
+#include <RunningAverage.h> //A lib to handle getting an average int, from http://playground.arduino.cc/Main/RunningAverage
+#include <Time.h>
 
 void setup()
 {
